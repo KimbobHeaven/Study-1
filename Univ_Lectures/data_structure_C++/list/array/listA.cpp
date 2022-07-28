03cd73eacd83db391f59ba5524082cefa01ca113
DevKimbob <devkimbob@gmail.com>
2022-07-28 13:24:13 +0900

    .

--- Univ_Lectures/data_structure_C++/list/array/listA.cpp
+++ Univ_Lectures/data_structure_C++/list/array/listA.cpp
@@ -1,24 +1,25 @@
-#include <iostream>
 #include "listA.h"
+
+#include <iostream>
 using namespace std;
 
 listArray::listArray() {
-	Count = 0;
+    Count = 0;
 }
 
-llistArray::istArray(const listArray& L) {
-	this->Count = L.Count;
-	for (int i = 0; i < this->Count, i++) {
-		this->Data[i] = L.Data[i];
-	}
+listArray::listArray(const listArray& L) {
+    this->Count = L.Count;
+    for (int i = 0; i < this->Count, i++) {
+        this->Data[i] = L.Data[i];
+    }
 }
 
 listArray::~listArray() {}
 
-void listArray::Insert(int Position, int Item);
-void listArray::Delete(int Position);
-void listArray::Retrieve(int Position, int& Item);
-bool listArray::IsEmpty();
-int listArray::Length();
+void listArray::Insert(int Position, int Item) {}
+void listArray::Delete(int Position) {}
+void listArray::Retrieve(int Position, int& Item) {}
+bool listArray::IsEmpty() {}
+int listArray::Length() {}
 
 "test string"

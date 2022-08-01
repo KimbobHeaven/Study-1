89ad16aba54de6155029abb03c1c1e374aa0ab3b
DevKimbob-OCI-uamd <devkimbob@gmail.com>
2022-08-01 16:26:45 +0000

    .

--- Univ_Lectures/data_structure_C++/tree/binary_search_tree/biSrchTree.h
+++ Univ_Lectures/data_structure_C++/tree/binary_search_tree/biSrchTree.h
@@ -1,1 +1,27 @@
-typedef struct tree
+typedef struct dataRecord {
+    int Key;
+} dataType;
+    
+typedef struct treeRecord {
+    dataType Data;
+    struct treeRecord* LChild;
+    struct treeRecord* RChild;
+} node;
+
+typedef node* Nptr;
+
+class BST {
+    public:
+    BST();
+    BST(const BST& B);
+    ~BST();
+    
+    Nptr Search(Nptr T, int Key);
+    Nptr Insert(Nptr T, int Key);
+    Nptr Delete(Nptr T, int Key);
+
+    void Traverse();
+
+    private:
+    Nptr Head;
+}
